8b0a39cbf814100aa6f96f5a16767f3ba060e77b
mrodriguezcimino <mrodriguezcimino@HXNB-1621>
2023-07-05 13:04:01 -0300

    Added comments

--- IdPDemo/Program.cs
+++ IdPDemo/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var isBuilder = builder.Services.AddIdentityServer();
+
+// client applications, or Relying Parties (RP)
 isBuilder.AddInMemoryClients(new List<Client>
 {
     new Client
@@ -34,19 +36,21 @@ isBuilder.AddInMemoryClients(new List<Client>
     }
 });
 
+// scopes available for clients to request
 isBuilder.AddInMemoryApiScopes(new List<ApiScope>
 {
     new ApiScope("app", "app"),
     new ApiScope("user", "user")
 });
 
+// user information that can be requested by the client
 isBuilder.AddInMemoryIdentityResources(new List<IdentityResource> 
 { 
     new IdentityResources.OpenId(),
     new IdentityResources.Profile()
 });
 
-
+// Add some users 
 isBuilder.AddTestUsers(new List<TestUser>
 { 
     new TestUser
@@ -58,8 +62,15 @@ isBuilder.AddTestUsers(new List<TestUser>
     }
 });
 
+// Private/Public key pair required to sign JWTs
 isBuilder.AddDeveloperSigningCredential();
 
+//I've broken down the IS4 configuration to showcase individual configurations,
+//but each of the methods above return the IS builder so a fluent syntax would chain them toghether
+// builder.Services.AddIdentityServer(...)
+//                 .AddInMemoryClients(...)
+//                 .AddInMemoryApiScopes(...)
+//                 .AddInMemoryIdentityResources(...);
 
 var app = builder.Build();
 
